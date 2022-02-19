68c0bd5970cc821782093b4f4b70d7aebb69ac26
sanwar786 <95489644+sanwar786@users.noreply.github.com>
2022-02-19 23:23:19 +0530

    Commit NLog

---
+++ LogFiles/Addition.cs
@@ -0,0 +1,35 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemoBatch102.LogFiles
+{
+    internal class Addition
+    {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+        public static void Sum(int first, int second)
+        {
+            if (first <= 0 || second <= 0)
+            {
+                //NLog.DebugInfo("Debug Unsuccessful due to wrong inputs");
+                //NLog.ErrorInfo("Providing wrong inputs");
+                //NLog.WarnInfo("Inputs should not be zero values");
+                logger.Debug("Debug Unsuccessful due to wrong inputs");
+                logger.Error("Providing wrong inputs");
+            }
+            else
+            {
+                int sum = first + second;
+                Console.WriteLine("sum:" + sum);
+                logger.Debug("Debug successful");
+                logger.Info("Successfully caluculated sum:" + sum);
+                //NLog.DebugInfo("Debug successful");
+                //NLog.InfoLevel("Successfully caluculated sum:"+sum);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using NLogDemoBatch102.LogFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemoBatch102
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition.Sum(12, 21);
+            Console.Read();
+        }
+    }
+}
